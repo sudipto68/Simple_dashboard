f039ec887ceb82a454aa06f2594c499117e4b4cb
sudipto68 <sudiptomitro2016@gmail.com>
2021-08-15 01:48:05 +0600

    new icon button added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import SideBar from "./Components/SideBar/Sidebar";
 
 function App() {
   return (
-    <div className="containe">
+    <>
       <div className="col-md-12">
         <div className="row">
           <SideBar />
           <Body />
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
